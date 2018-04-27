18b8381df4c5602d7e438022a7e1a1b995b086c3
Anton Sergeev <antonsergeev88@gmail.com>
2018-04-28 02:34:40 +0300

    Add should perform method implementation

--- PrepareForSegue/ASPrepareForSegueLoader.m
+++ PrepareForSegue/ASPrepareForSegueLoader.m
@@ -30,6 +30,26 @@ static void as_prepareForSegue_sender(UIViewController *self, SEL _cmd, UIStoryb
     }
 }
 
+static void as_shouldPerformSegueWithIdentifier_sender(UIViewController *self, SEL _cmd, NSString *identifier, id sender) {
+    if (!identifier.length) {
+        return;
+    }
+    NSString *shouldPerformSegueSelectorString = [NSString stringWithFormat:@"shouldPerform%@", identifier];
+    SEL shouldPerformSegueSelector = NSSelectorFromString(shouldPerformSegueSelectorString);
+    SEL originalShouldPerformSegueSelector = NSSelectorFromString(@"shouldPerformSegueWithIdentifier:sender:");
+    if ([self respondsToSelector:shouldPerformSegueSelector]) {
+        Method shouldPerformSegueMethod = class_getInstanceMethod(self.class, shouldPerformSegueSelector);
+        char *returnType = method_copyReturnType(shouldPerformSegueMethod);
+        Method originalShouldPerformSegueMethod = class_getInstanceMethod(self.class, originalShouldPerformSegueSelector);
+        char *originalReturnType = method_copyReturnType(originalShouldPerformSegueMethod);
+        if (!strcmp(returnType, originalReturnType)) {
+            [self performSelector:shouldPerformSegueSelector];
+        }
+        free(returnType);
+        free(originalReturnType);
+    }
+}
+
 @interface ASPrepareForSegueLoader : NSObject
 
 @end
@@ -38,9 +58,14 @@ static void as_prepareForSegue_sender(UIViewController *self, SEL _cmd, UIStoryb
 
 + (void)load {
     Class viewControllerClass = objc_getClass("UIViewController");
+
     SEL prepareForSegueSelector = NSSelectorFromString(@"prepareForSegue:sender:");
     Method prepareForSegueMethod = class_getInstanceMethod(viewControllerClass, prepareForSegueSelector);
     method_setImplementation(prepareForSegueMethod, (IMP)as_prepareForSegue_sender);
+
+    SEL shouldPerformSegueSelector = NSSelectorFromString(@"shouldPerformSegueWithIdentifier:sender:");
+    Method shouldPerformSegueMethod = class_getInstanceMethod(viewControllerClass, shouldPerformSegueSelector);
+    method_setImplementation(shouldPerformSegueMethod, (IMP)as_shouldPerformSegueWithIdentifier_sender);
 }
 
 @end
