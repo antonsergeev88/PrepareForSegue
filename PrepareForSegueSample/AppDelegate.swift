0382a324839534da81159c60a3ef63af89d663a5
Anton Sergeev <antonsergeev88@gmail.com>
2018-04-28 19:22:00 +0300

    Add target with Sample app

---
+++ PrepareForSegueSample/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  PrepareForSegueSample
+//
+//  Created by Anton Sergeev on 28.04.2018.
+//  Copyright © 2018 antonsergeev88. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
+}
+
