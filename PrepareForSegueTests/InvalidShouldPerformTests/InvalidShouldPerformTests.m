8fb7a4fe67ebaaac557321a8c9b06b193c7d27a3
Anton Sergeev <antonsergeev88@gmail.com>
2018-04-29 10:58:09 +0300

    Add should perform with no custom method performance tests

--- PrepareForSegueTests/InvalidShouldPerformTests/InvalidShouldPerformTests.m
+++ PrepareForSegueTests/InvalidShouldPerformTests/InvalidShouldPerformTests.m
@@ -35,12 +35,26 @@
     XCTAssertTrue(shouldPerformSegue);
 }
 
+- (void)testNotCallingShouldPerformSegueWithNoReturnValuePerformance {
+    [self measureBlock:^{
+        [self->_sourceViewController shouldPerformSegueWithIdentifier:@"TestingWithNoReturnValue"
+                                                               sender:self->_sourceViewController];
+    }];
+}
+
 - (void)testNotCallingShouldPerformSegueWithPointerReturnValue {
     BOOL shouldPerformSegue = [_sourceViewController shouldPerformSegueWithIdentifier:@"TestingWithPointerReturnValue"
                                                      sender:_sourceViewController];
     BOOL shouldPerformMethodWasCalled = _sourceViewController.shouldPerformMethodWasCalled;
     XCTAssertFalse(shouldPerformMethodWasCalled);
     XCTAssertTrue(shouldPerformSegue);
+}
+
+- (void)testNotCallingShouldPerformSegueWithPointerReturnValuePerformance {
+    [self measureBlock:^{
+        [self->_sourceViewController shouldPerformSegueWithIdentifier:@"TestingWithPointerReturnValue"
+                                                               sender:self->_sourceViewController];
+    }];
 }
 
 - (void)testNotCallingShouldPerformSegueWithObjectReturnValue {
@@ -51,6 +65,13 @@
     XCTAssertTrue(shouldPerformSegue);
 }
 
+- (void)testNotCallingShouldPerformSegueWithObjectReturnValuePerformance {
+    [self measureBlock:^{
+        [self->_sourceViewController shouldPerformSegueWithIdentifier:@"TestingWithObjectReturnValue"
+                                                               sender:self->_sourceViewController];
+    }];
+}
+
 - (void)testNotCallingShouldPerformSegueWithNoMethodForSelector {
     BOOL shouldPerformSegue = [_sourceViewController shouldPerformSegueWithIdentifier:@"TestingWithNoMethodForSelector"
                                                                                sender:_sourceViewController];
@@ -59,12 +80,26 @@
     XCTAssertTrue(shouldPerformSegue);
 }
 
+- (void)testNotCallingShouldPerformSegueWithNoMethodForSelectorPerformance {
+    [self measureBlock:^{
+        [self->_sourceViewController shouldPerformSegueWithIdentifier:@"TestingWithNoMethodForSelector"
+                                                               sender:self->_sourceViewController];
+    }];
+}
+
 - (void)testNotCallingShouldPerformSegueWithEmptyIdentifier {
     BOOL shouldPerformSegue = [_sourceViewController shouldPerformSegueWithIdentifier:@""
                                                                                sender:_sourceViewController];
     BOOL shouldPerformMethodWasCalled = _sourceViewController.shouldPerformMethodWasCalled;
     XCTAssertFalse(shouldPerformMethodWasCalled);
     XCTAssertTrue(shouldPerformSegue);
+}
+
+- (void)testNotCallingShouldPerformSegueWithEmptyIdentifierPerformance {
+    [self measureBlock:^{
+        [self->_sourceViewController shouldPerformSegueWithIdentifier:@""
+                                                               sender:self->_sourceViewController];
+    }];
 }
 
 @end
