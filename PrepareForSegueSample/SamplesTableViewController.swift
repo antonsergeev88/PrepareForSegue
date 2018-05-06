0c8958b389e1bf2a9f5684e6fc5b61fa32f5a290
Anton Sergeev <antonsergeev88@gmail.com>
2018-05-06 18:40:44 +0300

    Add prepare with sender in sample project

--- PrepareForSegueSample/SamplesTableViewController.swift
+++ PrepareForSegueSample/SamplesTableViewController.swift
@@ -14,17 +14,29 @@ class SamplesTableViewController: UITableViewController {
         print("PrepareForSegue: \(self.title!) is preparing for first sample. This method was called because it is exported in Objective-C with @objc and named \"prepareFor<SegueIdentifier>()\".")
     }
 
-    @objc func shouldPerformSecondSample() -> Bool {
-        print("PrepareForSegue: \(self.title!) is asked if it should perform second sample. This method was called because it is exported in Objective-C with @objc, named \"shouldPerform<SegueIdentifier>()\" and returns Bool.")
-        return true
+    @objc func prepareForSecondSample() {
+        fatalError("Will never called")
+    }
+
+    @objc func prepareForSecondSample(sender: Any?) {
+        print("PrepareForSegue: \(self.title!) is preparing for second sample. This method was called because it is exported in Objective-C with @objc and named \"prepareFor<SegueIdentifier>(sender: Any?)\". This method has \"sender\" parameter and has higher priority than method without one.")
     }
 
     @objc func shouldPerformThirdSample() -> Bool {
-        print("PrepareForSegue: \(self.title!) is asked if it should perform third sample. It returns \"false\", so preparing method will never called.")
-        return false
+        print("PrepareForSegue: \(self.title!) is asked if it should perform third sample. This method was called because it is exported in Objective-C with @objc, named \"shouldPerform<SegueIdentifier>()\" and returns Bool.")
+        return true
     }
 
     @objc func prepareForThirdSample() {
+        print("PrepareForSegue: \(self.title!) is preparing for third sample.")
+    }
+
+    @objc func shouldPerformFourthSample() -> Bool {
+        print("PrepareForSegue: \(self.title!) is asked if it should perform fourth sample. It returns \"false\", so preparing method will never called.")
+        return false
+    }
+
+    @objc func prepareForFourthSample() {
         fatalError("Will never called")
     }
 
